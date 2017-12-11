c3929cb63830667bffb548b02558209bb3e6e83a
Shuma Yoshioka <s64.stdio@gmail.com>
2017-12-12 02:55:39 +0900

    Add UniversalCookieRequest i/f

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,12 @@
 import * as express from 'express';
+import Cookies from 'universal-cookie';
 
 declare function universalCookieMiddleware(): express.Handler;
-declare namespace universalCookieMiddleware {}
+declare namespace universalCookieMiddleware {
+
+  export interface UniversalCookieRequest {
+    universalCookies?: Cookies
+  }
+
+}
 export = universalCookieMiddleware
